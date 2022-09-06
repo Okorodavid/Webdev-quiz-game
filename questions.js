e8e382110236ad318dc2f542a57908e8d2bec3e0
Okoro David <kdelicious062@gmail.com>
2022-09-06 15:23:23 +0100

    first commit

---
+++ questions.js
@@ -0,0 +1,168 @@
+let questions = [
+    {
+    numb: 1,
+    question: "Pick the odd Git hub term",
+    answer: "Git slap(when you have bug)",
+    options: [
+      "Git slap(when you have bug)",
+      "Git Push",
+      "Git add .",
+      "Git Commit"
+    ]
+  },
+    {
+    numb: 2,
+    question: "getElementbyId is a?",
+    answer: "Function",
+    options: [
+      "Variable",
+      "Function",
+      "Boolean",
+      "String"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What does PHP stand for?",
+    answer: "Hypertext Preprocessor",
+    options: [
+      "Hypertext Preprocessor",
+      "Hypertext Programming",
+      "Hypertext Preprogramming",
+      "Hometext Preprocessor"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What does SQL stand for?",
+    answer: "Structured Query Language",
+    options: [
+      "Stylish Question Language",
+      "Stylesheet Query Language",
+      "Statement Question Language",
+      "Structured Query Language"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What does DOM stand for?",
+    answer: "Document Object Model",
+    options: [
+      "Download Object Mass",
+      "Do Onclick Maximum",
+      "Document Object Model",
+      "Document Object Module"
+    ]
+  },
+    {
+        numb: 6,
+        question: "What does HTML stand for?",
+        answer: "Hyper Text Markup Language",
+        options: [
+        "Hyper Text Preprocessor",
+        "Hyper Text Markup Language",
+        "Hyper Text Multiple Language",
+        "Hyper Tool Multi Language"
+        ]
+    },
+    {
+        numb: 7,
+        question: "What does CSS stand for?",
+        answer: "Cascading Style Sheet",
+        options: [
+          "Common Style Sheet",
+          "Colorful Style Sheet",
+          "Computer Style Sheet",
+          "Cascading Style Sheet"
+        ]
+      },
+    {
+    numb: 8,
+    question: "What does XML stand for?",
+    answer: "eXtensible Markup Language",
+    options: [
+      "eXtensible Markup Language",
+      "eXecutable Multiple Language",
+      "eXTra Multi-Program Language",
+      "eXamine Multiple Language"
+    ]
+  },
+    {
+    numb: 9,
+    question: "Which of these are enclosed in inverted commas?",
+    answer: "Strings",
+    options: [
+      "Strings",
+      "Variables",
+      "Attributes",
+      "Formulars"
+    ]
+  },
+    {
+    numb: 10,
+    question: "What do you use to check for bugs in js?",
+    answer: "Console.log",
+    options: [
+      "Formulars",
+      "Live server",
+      "Use insecticide",
+      "Console.log"
+    ]
+  },
+    {
+    numb: 11,
+    question: "Units in css, except?",
+    answer: "One cup",
+    options: [
+      "em",
+      "rem",
+      "One cup",
+      "px"
+    ]
+  },
+    {
+    numb: 12,
+    question: "Can you style the 'a' element",
+    answer: "Yes",
+    options: [
+      "Yes",
+      "Maybe",
+      "I have forgotten",
+      "No"
+    ]
+  },
+    {
+    numb: 13,
+    question: "What do you do 1st when coding Js?",
+    answer: "Define how you want the fuctionality",
+    options: [
+      "Define how you want the fuctionality",
+      "Drink water",
+      "Do Fasting and prayer",
+      "eXamine Multiple Language"
+    ]
+  },
+    {
+    numb: 14,
+    question: "What % of border-radius makes a circle?",
+    answer: "50%",
+    options: [
+      "100%",
+      "20%",
+      "50%",
+      "0%"
+    ]
+  },
+    {
+    numb: 15,
+    question: "The Variable attribute in js is used for",
+    answer: "Storing Data",
+    options: [
+      "I have forgotten",
+      "Solving Maths",
+      "Making Games",
+      "Storing Data"
+    ]
+  },
+
+];
